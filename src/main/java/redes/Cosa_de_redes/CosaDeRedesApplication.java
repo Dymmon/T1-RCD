8c103f7464eec809be5822f71f0f27765dc37830
Scalim <javier.garridor@utem.cl>
2020-09-17 14:03:18 -0300

    rest java con .jar

---
+++ src/main/java/redes/Cosa_de_redes/CosaDeRedesApplication.java
@@ -0,0 +1,13 @@
+package redes.Cosa_de_redes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosaDeRedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosaDeRedesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/redes/Cosa_de_redes/CosaDeRedesApplicationTests.java
@@ -0,0 +1,13 @@
+package redes.Cosa_de_redes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosaDeRedesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
